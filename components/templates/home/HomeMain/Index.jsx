ccaa3ef192f502bd1a130dfe4ec14c0f73302c68
semilunar <katyanikitina.me@yandex.ru>
2021-10-26 01:10:32 +0300

    feat: new text

--- components/templates/home/HomeMain/Index.jsx
+++ components/templates/home/HomeMain/Index.jsx
@@ -7,13 +7,9 @@ const HomeMain = () => {
       <div className={styles.paragraphWrapper}>
         <P
           text={
-            "This key is a part of the story. You have just one. Find people with other keys to unveil the story behind them."
+            "Light-responsive butterflies. Iron sticks with which you can beat a person or a bush, Symbiosis of a mask and glasses with minus glasses, Expansion of consciousness, techniques for going into oblivion, Leaving home early, primarily caring for others Feeding hummingbirds from a pipette with a special solution, A shark biting off a leg to the knee Backstage passage combined with Mimosa cocktail, Dress caught on the fence when trying to leave the territory of a country house, More than an hour late due to the cancellation of the train, Winning the lottery, A goodbye kiss I didn't think of as my last, Runaway milk, Alienation in the desert, A port city with cheerful people, Selling grandma's silver, The war that started out so ridiculous and hasn't ended, Distribution of books and drugs, Dots of brilliant green all over the face, Knots at the ends of the strings of the jacket, Feet that can stand to stand on nails, Doing this in the dark, The treasure that no one is looking for, They turned off the hot water, I fell down the stairs and broke my nose, At some point it all went wrong, Dressed up and went out, The grandmother who lost her memory and asks passers-by where is her home, I have never had a cat and I haven't hugged anyone for a long time, say, 3 minutes."
           }
         />
-      </div>
-      <div className={styles.paragraphWrapper}>
-        <P text={"Hint to find the holder of the previous key:"} />
-        <P text={"D#(217, 33, 208)(dot)com"} />
       </div>
     </div>
   );
